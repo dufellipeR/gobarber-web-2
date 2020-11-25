397b826724f10a1bc1361c1255a116c891e582dc
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-24 22:47:22 -0300

    Login Page

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,7 +3,6 @@ import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
-  color: #3a3a3a;
   max-width: 450px;
   line-height: 56px;
 
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,19 +9,25 @@ export default createGlobalStyle`
     }
 
     body {
-
+      background: #312e38;
+      color: #fff;
       -webkit-font-smoothing: antialiased;
     }
 
     body, input, button {
-      font: 16px Roboto,sans-serif;
+      font: 16px 'Roboto Slab', serif;
+
     }
 
     /* Centraliza o conteudo */
-    #root {
+    /* #root {
       max-width: 960px;
       margin: 0 auto;
       padding: 40px 20px;
+    } */
+
+    h1,h2,h3,h4,h5,h6, strong {
+      font-weight: 500;
     }
 
     button {
