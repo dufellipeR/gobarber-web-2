cbc025d7046d898c5bb34bcd93e1921675b7a6dc
dufellipeR <eduardo.fellipe30@gmail.com>
2020-11-30 22:56:52 -0300

    Aunthentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
+import { AuthProvider } from './hooks/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
